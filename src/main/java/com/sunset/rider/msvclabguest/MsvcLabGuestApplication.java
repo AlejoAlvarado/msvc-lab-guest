33a5e5071ddc6b1460f1b73021100b92a6f9a9ff
ALEJOMELA <nuestrocanal1@gmail.com>
2023-12-14 17:31:48 -0500

    Added pom and spring project

---
+++ src/main/java/com/sunset/rider/msvclabguest/MsvcLabGuestApplication.java
@@ -0,0 +1,13 @@
+package com.sunset.rider.msvclabguest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcLabGuestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcLabGuestApplication.class, args);
+	}
+
+}
